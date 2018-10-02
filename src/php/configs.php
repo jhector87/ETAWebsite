365b5adf7e7e443f015aaef8d5ff6e17711451bf
Jonathan Hector <louissebatienjonathan.hector@students.bfh.ch>
2018-10-02 15:09:09 +0200

    Home page update

---
+++ src/php/configs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jpro
+ * Date: 18.09.18
+ * Time: 17:54
+ */
+ function getConfigs() {
+ $user = "root";
+	$pwd = "";
+$database = "phpCourse";
+         $conn = new mysqli("127.0.0.1", $user, $pwd, $database);
+			 }
+
+?>
