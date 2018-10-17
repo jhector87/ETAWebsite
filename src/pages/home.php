067a4d11cdbaa61df39c7df93f8f0ec562ab4ef9
Jonathan Hector <louissebatienjonathan.hector@students.bfh.ch>
2018-10-17 08:52:11 +0200

    Updated content.
    Added login page and videos removed.

--- src/pages/home.php
+++ src/pages/home.php
@@ -7,12 +7,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script>
+$(document).ready(function(){
+        $('#logo-animation').fadeIn(3000).removeClass('hidden');
+			});
+</script>
 </head>
 
-<body class="container-fluid" id="id">
+<body class="container-fluid">
 	<div class="fixed-bckg" id="rectangle">
 
-	<?php include("../pages/navigation.php"); ?>
+	<?php include("navigation.php")?>
 
 
 	<!-- <div class="content">
@@ -20,20 +25,21 @@
     <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei.</p>
   </div> -->
 
-<div class="logo2"><img src="../../src/res/icons/png/002-antenna.png"></div>
+<div class="logo2" id="logo-animation"><img src="../../src/res/icons/png/002-antenna.png"><br>
+<h2 style="color:white; background: rgba(0.0,0.0, 0.0, 0.5)">Be as efficient as if it was your second nature.</h2>
+</div>
 	</div>
 
 	<main>
 
 		<div class="responsive">
 			<div class="gallery">
-				<a target="_blank" href="../res/images/1086695-computer-technology/png/005-cloud-server.png">
-					<img src="../res/images/1086695-computer-technology/png/005-cloud-server.png" alt="Cinque Terre" width="600" height="400">
+				<a target="_blank" href="../../src/res/images/png/1086695-computer-technology/png/005-cloud-server.png">
+					<img src="../res/images/1086695-computer-technology/png/005-cloud-server.png" alt="CLOUD SERVER" width="600" height="400">
 				</a>
 				<div class="desc">
 					<h3>Database</h3>
-					<p>Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor idnibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. <br><br>Vivamus sagittis lacus
-					vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+					<p>Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor idnibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. <br><br>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
 				</div>
 			</div>
 		</div>
@@ -41,8 +47,8 @@
 
 		<div class="responsive">
 			<div class="gallery">
-				<a target="_blank" href="../res/images/1086695-computer-technology/png/008-data-link.png">
-					<img src="../res/images/1086695-computer-technology/png/008-data-link.png" alt="Forest" width="600" height="400">
+				<a target="_blank" href="/src/res/icons/png/003-apartment.png">
+					<img src="../res/images/1086695-computer-technology/png/008-data-link.png" alt="DATA" width="600" height="400">
 				</a>
 				<div class="desc">
 					<h3>File Sharing</h3>
@@ -54,8 +60,8 @@
 
 		<div class="responsive">
 			<div class="gallery">
-				<a target="_blank" href="../res/images/1086695-computer-technology/png/030-shield.png">
-					<img src="../res/images/1086695-computer-technology/png/030-shield.png" alt="Northern Lights" width="600" height="400">
+				<a>
+					<img src="../res/images/1086695-computer-technology/png/030-shield.png" alt="SECURITY" width="600" height="400">
 				</a>
 				<div class="desc">
 					<h3>Security</h3>
@@ -71,7 +77,7 @@
 
 		<div class="responsive">
 			<div class="gallery">
-				<a target="_blank" href="../res/images/1086695-computer-technology/png/032-network-1.png">
+				<a>
 					<img src="../res/images/1086695-computer-technology/png/032-network-1.png" alt="Network" width="600" height="400">
 				</a>
 				<div class="desc">
@@ -90,9 +96,9 @@
 
 		<!-- Creates a request access -->
 		<div class="quickrequest oval" id="#top">
-			<form action="../../src/config/request.php" method="post">
-				<input type="text" placeholder="Please enter your email..." name="email"><br>
-				<span class="requestMsg"><input type="text" placeholder="Enter your request here..." name="request"></span><br>
+			<form action="login.php" method="post">
+				<p><input type="text" placeholder="Please enter your email..." name="email"></p>
+				<p><textarea name="comment" placeholder="Please enter a short message here describing your request..." rows="5" cols="60" style="border-color: #66CC00;"></textarea></p>
 				<br>
 				<a href=accept="../../src/config/request.php" /><input type="submit" value="Submit request"></a><br>
 				<a style="margin-top: 10px;"><img src="../res/icons/rocket-2.png"></a>
@@ -103,7 +109,7 @@
 
 	</main>
 
-	<footer>
+	<footer style="display: center;">
 		<?php include("../pages/footer.php"); ?>
 	</footer>
 </body>
